8a0f6163776b6d388d314b129c3bb015632d8d64
annazapolska <anna.pzapolska@gmail.com>
2023-06-09 19:34:28 +0900

    added javadoc for CSVFileCalculator

--- app/src/main/java/edu/handong/csee/java/hw5/thread/CSVFileCalculator.java
+++ app/src/main/java/edu/handong/csee/java/hw5/thread/CSVFileCalculator.java
@@ -15,14 +15,27 @@ import org.apache.commons.csv.CSVRecord;
 import edu.handong.csee.java.hw5.clioptions.OptionHandler;
 import edu.handong.csee.java.hw5.engines.Computable;
 
+
+/**
+ * Performs calculations on a CSV file when file directory is provided.
+ * This class provides methods to calculate values based on a CSV file.
+ * Class uses threads to operate on multiple files simultaneously
+ * @version 1.0
+ */
 public class CSVFileCalculator implements Runnable {
 	private String inputFilePath;
 	private String outputFilePath;
 	private String outputFileName;
-    String engineName;
-	Computable engine;
+    private String engineName;
 	private ArrayList<ArrayList<String>> csvData;
 	
+	
+	/**
+     * Constructs a new CSVFileCalculator instance.
+     * @param engineName         the name of the calculation engine
+     * @param inputFileDirectory the directory of the input CSV file
+     * @param outputFilePath     the output CSV file path
+     */
 	public CSVFileCalculator(String engineName, String inputFileDirectory, String outputFilePath){
 		this.inputFilePath = inputFilePath;
 		this.outputFilePath = outputFilePath;
@@ -30,6 +43,91 @@ public class CSVFileCalculator implements Runnable {
 		this.engineName = engineName;
 	}
 	
+	/**
+     * Retrieves the input file path.
+     * @return the input file path
+     */
+	public String getInputFilePath() {
+		return inputFilePath;
+	}
+
+	/**
+     * Sets the input file path.
+     * @param inputFilePath the input file path to set
+     */
+	public void setInputFilePath(String inputFilePath) {
+		this.inputFilePath = inputFilePath;
+	}
+
+	/**
+     * Retrieves the output file path.
+     * @return the output file path
+     */
+	public String getOutputFilePath() {
+		return outputFilePath;
+	}
+
+	/**
+     * Sets the output file path.
+     * @param outputFilePath the output file path to set
+     */
+	public void setOutputFilePath(String outputFilePath) {
+		this.outputFilePath = outputFilePath;
+	}
+
+	/**
+     * Retrieves the output file name.
+     * @return the output file name
+     */
+	public String getOutputFileName() {
+		return outputFileName;
+	}
+
+	/**
+     * Sets the output file name.
+     * @param outputFileName the output file name to set
+     */
+	public void setOutputFileName(String outputFileName) {
+		this.outputFileName = outputFileName;
+	}
+
+	/**
+     * Retrieves the calculation engine name.
+     * @return the calculation engine name
+     */
+	public String getEngineName() {
+		return engineName;
+	}
+
+	/**
+     * Sets the calculation engine name.
+     * @param engineName the calculation engine name to set
+     */
+	public void setEngineName(String engineName) {
+		this.engineName = engineName;
+	}
+
+	/**
+     * Retrieves the CSV data.
+     * @return the CSV data
+     */
+	public ArrayList<ArrayList<String>> getCsvData() {
+		return csvData;
+	}
+
+	/**
+     * Sets the CSV data.
+     * @param csvData the CSV data to set
+     */
+	public void setCsvData(ArrayList<ArrayList<String>> csvData) {
+		this.csvData = csvData;
+	}
+	
+	/**
+     * Reads a CSV file and returns the data as a 2D ArrayList.
+     * @param filePath the path of the CSV file to read
+     * @return the data read from the CSV file as a 2D ArrayList
+     */
 	public ArrayList<ArrayList<String>> readCSV(String filePath){
 		ArrayList<ArrayList<String>> arrayList = new ArrayList<>();
 		
@@ -68,7 +166,11 @@ public class CSVFileCalculator implements Runnable {
 		return arrayList;
 	}
 	
-	
+	/**
+     * Writes the CSV data to a file.
+     * @param filePath the path of the output CSV file
+     * @param csvData the CSV data to write
+     */
 	public void writeCSV(String filePath, ArrayList<ArrayList<String>> csvData) {
 	    try (CSVPrinter csvPrinter = new CSVPrinter(new FileWriter(filePath), CSVFormat.DEFAULT.withQuote(null))) {
 
@@ -83,6 +185,12 @@ public class CSVFileCalculator implements Runnable {
 	    }
 	}
 	
+	/**
+     * Performs calculations on the CSV data based on the calculation engine.
+     * The calculated values are updated in the CSV data.
+     * @param engineName the name of the calculation engine
+     * @param csvData the CSV data to perform calculations on
+     */
 	public void calculate(String engineName, ArrayList<ArrayList<String>> csvData) { //a method for calculating SQRT, MAX, and MIN tasks
 	    ArrayList<ArrayList<String>> updatedList = new ArrayList<>();
 	    
