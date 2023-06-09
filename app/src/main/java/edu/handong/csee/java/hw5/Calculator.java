bf203aa472fb7da76f052a21ec8b44eeebb9a962
annazapolska <anna.pzapolska@gmail.com>
2023-06-09 17:57:05 +0900

    added the calculate function for the CSV calculator class and modified the calculator main function for when inputfilepath is a filePath and not a directory

--- app/src/main/java/edu/handong/csee/java/hw5/Calculator.java
+++ app/src/main/java/edu/handong/csee/java/hw5/Calculator.java
@@ -148,16 +148,18 @@ public class Calculator {
 				engine.compute();
 			}
 			
-			// ...
+
 
 			// if the data input and output file paths are provided
 			else if (optionHandler.getDataInputFilePath() != null && optionHandler.getDataOutputFilePath() != null && optionHandler.getInputValues() == null) {
 			    int numThreads = 10;
 			    Thread[] threads = new Thread[numThreads];
+			    ArrayList<CSVFileCalculator> calculators = new ArrayList<>(); //array of CSVFileCalculator instances for each of the files
 
 			    File inputFile = new File(optionHandler.getDataInputFilePath());
 			    File outputFile = new File(optionHandler.getDataOutputFilePath());
 			    
+			    //if the inputPath is a directory
 			    if (inputFile.isDirectory()) {
 			        // Process all files in the directory
 			        File[] files = inputFile.listFiles();
@@ -166,107 +168,92 @@ public class Calculator {
 			            for (File file : files) {
 			                if (file.isFile()) {
 			                    // Create a CSVFileCalculator instance for each file
-			                    CSVFileCalculator calculator = new CSVFileCalculator(file.getAbsolutePath(), outputFile.getAbsolutePath());
+			                    CSVFileCalculator calculator = new CSVFileCalculator(engineName, file.getAbsolutePath(), outputFile.getAbsolutePath());
+			                    calculators.add(calculator); //add new calculator to the array of CSVFile Calculators
 			                    Thread thread = new Thread(calculator);
 			                    thread.start();
 			                }
 			            }
 			        }
 			    
+			        //if the input path is filePath
 			    } else if (inputFile.isFile()) {
-			        // Process a single file
-			        CSVFileCalculator calculator = new CSVFileCalculator( inputFile.getAbsolutePath(),  outputFile.getAbsolutePath());
-			        threads[0] = new Thread(calculator);
-			        threads[0].start();
+			    	
+					//when SQRT engine is used
+			    	if (engineName.equals("SQRT")) {
+						ArrayList<String> list = FileManager.readLinesFromATxtFile(optionHandler.getDataInputFilePath());
+						ArrayList<String> updatedList = new ArrayList<String>();
+						//adding headers to the list
+						updatedList.add(list.get(0));
+
+						//adding the rows
+						for (int i=1; i<list.size(); i++) {
+							StringBuilder row = new StringBuilder();
+							String[] array=list.get(i).split(",");
+							for (int j=0; j<array.length; j++) {
+								String[] inputNumber = {engineName, array[j]};
+								engine.setInput(inputNumber);
+								engine.compute();
+								
+								if (j == array.length - 1) {
+						            row.append(String.valueOf(engine.getResult()));
+						        } else {
+						            row.append(String.valueOf(engine.getResult())).append(",");
+						        }
+								
+							}
+							updatedList.add(row.toString());
+						}
+						
+						FileManager.writeAtxtFile(optionHandler.getDataOutputFilePath(), updatedList);
+					}
+
+					//when the Max option is used
+					else if (engineName.equals("MAX")) {
+						ArrayList<String> list = FileManager.readLinesFromATxtFile(optionHandler.getDataInputFilePath());
+						ArrayList<String> updatedList = new ArrayList<String>();
+					    
+						updatedList.add(list.get(0)+","+"MAX");
+					    
+						for (int i=1; i<list.size(); i++) {
+							String[] array = list.get(i).split(",");
+							String[] inputNumber = new String[array.length + 1];
+							inputNumber[0] = engineName;
+							// Copy the original elements starting from index 0 to index 1
+							System.arraycopy(array, 0, inputNumber, 1, array.length);
+							engine.setInput(inputNumber);
+							engine.compute();
+							int result = (int) engine.getResult();
+							updatedList.add(list.get(i)+","+String.valueOf(result));
+						}
+						
+						FileManager.writeAtxtFile(optionHandler.getDataOutputFilePath(), updatedList);
+					}
+					
+			    	//when Min option is used
+					else if (engineName.equals("MIN")) {
+						ArrayList<String> list = FileManager.readLinesFromATxtFile(optionHandler.getDataInputFilePath());
+						ArrayList<String> updatedList = new ArrayList<String>();
+					    
+						updatedList.add(list.get(0)+","+"MIN");
+					    
+						for (int i=1; i<list.size(); i++) {
+							String[] array = list.get(i).split(",");
+							String[] inputNumber = new String[array.length + 1];
+							inputNumber[0] = engineName;
+							// Copy the original elements starting from index 0 to index 1
+							System.arraycopy(array, 0, inputNumber, 1, array.length);
+							engine.setInput(inputNumber);
+							engine.compute();
+							int result = (int) engine.getResult();
+							updatedList.add(list.get(i)+","+String.valueOf(result));
+						}
+						
+						FileManager.writeAtxtFile(optionHandler.getDataOutputFilePath(), updatedList);
+					}
+
 			    }
 			}
-
-
-			//when input and output files are provided for the SQRT engine
-			else if (engineName.equals("SQRT") && optionHandler.getDataInputFilePath()!=null && optionHandler.getDataOutputFilePath()!=null) {
-				if(optionHandler.getInputValues()!=null) {
-					optionHandler.printHelp(options);
-					System.exit(1);
-				}
-				ArrayList<String> list = FileManager.readLinesFromATxtFile(optionHandler.getDataInputFilePath());
-				ArrayList<String> updatedList = new ArrayList<String>();
-				//adding headers to the list
-				updatedList.add(list.get(0));
-
-				//adding the rows
-				for (int i=1; i<list.size(); i++) {
-					StringBuilder row = new StringBuilder();
-					String[] array=list.get(i).split(",");
-					for (int j=0; j<array.length; j++) {
-						String[] inputNumber = {engineName, array[j]};
-						engine.setInput(inputNumber);
-						engine.compute();
-						
-						if (j == array.length - 1) {
-				            row.append(String.valueOf(engine.getResult()));
-				        } else {
-				            row.append(String.valueOf(engine.getResult())).append(",");
-				        }
-						
-					}
-					updatedList.add(row.toString());
-				}
-				
-				FileManager.writeAtxtFile(optionHandler.getDataOutputFilePath(), updatedList);
-			}
-
-			//when the Max option is used
-			else if (engineName.equals("MAX") && optionHandler.getDataInputFilePath()!=null && optionHandler.getDataOutputFilePath()!=null) {
-				if(optionHandler.getInputValues()!=null) {
-					optionHandler.printHelp(options);
-					System.exit(1);
-				}
-				ArrayList<String> list = FileManager.readLinesFromATxtFile(optionHandler.getDataInputFilePath());
-				ArrayList<String> updatedList = new ArrayList<String>();
-			    
-				updatedList.add(list.get(0)+","+"MAX");
-			    
-				for (int i=1; i<list.size(); i++) {
-					String[] array = list.get(i).split(",");
-					String[] inputNumber = new String[array.length + 1];
-					inputNumber[0] = engineName;
-					// Copy the original elements starting from index 0 to index 1
-					System.arraycopy(array, 0, inputNumber, 1, array.length);
-					engine.setInput(inputNumber);
-					engine.compute();
-					int result = (int) engine.getResult();
-					updatedList.add(list.get(i)+","+String.valueOf(result));
-				}
-				
-				FileManager.writeAtxtFile(optionHandler.getDataOutputFilePath(), updatedList);
-			}
-			
-			else if (engineName.equals("MIN") && optionHandler.getDataInputFilePath()!=null && optionHandler.getDataOutputFilePath()!=null) {
-				if(optionHandler.getInputValues()!=null) {
-					optionHandler.printHelp(options);
-					System.exit(1);
-				}
-				ArrayList<String> list = FileManager.readLinesFromATxtFile(optionHandler.getDataInputFilePath());
-				ArrayList<String> updatedList = new ArrayList<String>();
-			    
-				updatedList.add(list.get(0)+","+"MIN");
-			    
-				for (int i=1; i<list.size(); i++) {
-					String[] array = list.get(i).split(",");
-					String[] inputNumber = new String[array.length + 1];
-					inputNumber[0] = engineName;
-					// Copy the original elements starting from index 0 to index 1
-					System.arraycopy(array, 0, inputNumber, 1, array.length);
-					engine.setInput(inputNumber);
-					engine.compute();
-					int result = (int) engine.getResult();
-					updatedList.add(list.get(i)+","+String.valueOf(result));
-				}
-				
-				FileManager.writeAtxtFile(optionHandler.getDataOutputFilePath(), updatedList);
-			}
-
-			//if the file path is provided
 
 			//all the other scenarios 
 			else {
--- app/src/main/java/edu/handong/csee/java/hw5/thread/CSVFileCalculator.java
+++ app/src/main/java/edu/handong/csee/java/hw5/thread/CSVFileCalculator.java
@@ -1,6 +1,5 @@
 package edu.handong.csee.java.hw5.thread;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -20,11 +19,15 @@ public class CSVFileCalculator implements Runnable {
 	private String inputFilePath;
 	private String outputFilePath;
 	private String outputFileName;
+    String engineName;
+	Computable engine;
+	private ArrayList<ArrayList<String>> csvData;
 	
-	public CSVFileCalculator(String inputFileDirectory, String outputFilePath){
+	public CSVFileCalculator(String engineName, String inputFileDirectory, String outputFilePath){
 		this.inputFilePath = inputFilePath;
 		this.outputFilePath = outputFilePath;
 		this.outputFileName = inputFilePath + "-" + outputFilePath + ".csv";
+		this.engineName = engineName;
 	}
 	
 	public ArrayList<ArrayList<String>> readCSV(String filePath){
@@ -80,26 +83,90 @@ public class CSVFileCalculator implements Runnable {
 	    }
 	}
 	
-	public void calculate(String engineName, ArrayList<ArrayList<String>> csvData, Computable engine ) { //a method for calculating SQRT, MAX, and MIN tasks
-		if (engineName=="SQRT") {
-			for (int i=1; i<csvData.size(); i++) {
-				ArrayList<String> row = csvData.get(i);
-				for (int j=1; j<row.size(); j++) {
-					String[] inputNumber = {engineName, row.get(j)};
-					//engine.setInput(inputNumber);
-					engine.compute();
-				}
-						
-			}
-				
-		}
+	public void calculate(String engineName, ArrayList<ArrayList<String>> csvData) { //a method for calculating SQRT, MAX, and MIN tasks
+	    ArrayList<ArrayList<String>> updatedList = new ArrayList<>();
+	    
+	    
+	    //if the engine is SQRT
+	    if (engineName.equals("SQRT")) {
+	        ArrayList<String> headerRow = csvData.get(0);
+	        updatedList.add(headerRow); // Append the header row as it is
+	        
+	        for (int i = 1; i < csvData.size(); i++) {
+	            ArrayList<String> row = csvData.get(i);
+	            ArrayList<String> updatedRow = new ArrayList<>();
+	            
+	            for (int j = 0; j < row.size(); j++) {
+	                String value = row.get(j);
+	                try {
+	                    double number = Double.parseDouble(value);
+	                    double sqrt = Math.sqrt(number);
+	                    updatedRow.add(String.valueOf(sqrt));
+	                } catch (NumberFormatException e) {
+	                    // Handle non-numeric values if needed
+	                    updatedRow.add(value);
+	                }
+	            }
+	            
+	            updatedList.add(updatedRow);
+	        }
+	    }
+	    
+	else if (engineName.equals("MIN")) {
+        ArrayList<String> headerRow = csvData.get(0);
+        headerRow.add("MIN"); // Add "MIN" header at the end
+        updatedList.add(headerRow); // Append the header row as it is
+        
+        for (int i = 1; i < csvData.size(); i++) {
+            ArrayList<String> row = csvData.get(i);
+            ArrayList<String> updatedRow = new ArrayList<>(row);
+            
+            double min = Double.POSITIVE_INFINITY;
+            for (int j = 0; j < row.size(); j++) {
+                String value = row.get(j);
+                try {
+                    double number = Double.parseDouble(value);
+                    min = Math.min(min, number);
+                } catch (NumberFormatException e) {
+                    // Handle non-numeric values if needed
+                }
+            }
+            
+            updatedRow.add(String.valueOf(min)); // Add min value at the end
+            updatedList.add(updatedRow);
+        }
+    } else if (engineName.equals("MAX")) {
+        ArrayList<String> headerRow = csvData.get(0);
+        headerRow.add("MAX"); // Add "MAX" header at the end
+        updatedList.add(headerRow); // Append the header row as it is
+        
+        for (int i = 1; i < csvData.size(); i++) {
+            ArrayList<String> row = csvData.get(i);
+            ArrayList<String> updatedRow = new ArrayList<>(row);
+            
+            double max = Double.NEGATIVE_INFINITY;
+            for (int j = 0; j < row.size(); j++) {
+                String value = row.get(j);
+                try {
+                    double number = Double.parseDouble(value);
+                    max = Math.max(max, number);
+                } catch (NumberFormatException e) {
+                    // Handle non-numeric values if needed
+                }
+            }
+            
+            updatedRow.add(String.valueOf(max)); // Add max value at the end
+            updatedList.add(updatedRow);
+        }
+    }
 	}
 		
 	@Override
 	public void run() {
-		
-		 
 		// TODO Auto-generated method stub
+		this.readCSV(inputFilePath);
+		this.calculate(engineName, csvData);
+        this.writeCSV(outputFileName, csvData);
 		
 	}
 
